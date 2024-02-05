92d92ac0693a6be036a9523d14dd71a8cc3991f6
kkub <krzysztof.kubieniec@hotmail.com>
2024-02-06 00:07:15 +0100

    moved constant values to file

--- car.py
+++ car.py
@@ -4,66 +4,64 @@ import pygame
 import pymunk
 import pymunk.pygame_util
 
+import constants
+
 
 class Car:
     def __init__(self, space, width, height, position=(100, 100), angle=0):
         self.collision_type = 1
         self.space = space
-        self.car_body = self.create_car_body(position, angle, width, height, self.collision_type)
-        self.car_shape = self.create_car_shape(self.car_body, width, height, self.collision_type)
+        self.car_body = self.create_car_body(position, angle, width, height)
+        self.car_shape = self.create_car_shape(self.car_body, width, height)
         self.car_image = pygame.image.load("car.png")
-        self.front_sensor_start = pymunk.Vec2d(0, 0)  # Convert to Vec2d
-        self.sensor_length = 100  # Length of the sensor lines
+        self.front_sensor_start = pymunk.Vec2d(0, 0)
         space.add(self.car_body, self.car_shape)
         self.sensor_shapes = []
-        self.sensors_number = 3
         self.add_sensors()
 
 
 
     @staticmethod
-    def create_car_body(position, angle, width, height, collision_type):
+    def create_car_body(position, angle, width, height):
         mass = 1
         moment = pymunk.moment_for_box(mass, (width / 40, height / 30))
         car_body = pymunk.Body(mass, moment)
         car_body.position = position
         car_body.angle = angle
-        car_body.collision_type = collision_type
+        car_body.collision_type = constants.CAR_COLLISION_TYPE
         return car_body
 
     @staticmethod
-    def create_car_shape(car_body, width, height, collision_type):
+    def create_car_shape(car_body, width, height):
         car_shape = pymunk.Poly.create_box(car_body, (width / 40, height / 30))
         car_shape.friction = 0
-        car_shape.collision_type = collision_type
+        car_shape.collision_type = constants.CAR_COLLISION_TYPE
         return car_shape
 
     def add_sensors(self):
-        sensor_length = 100
-        sensor_width = 5
         offset_distance = 0
-        angle_step = 180 / (self.sensors_number - 1) if self.sensors_number > 1 else 0
+        sensors_number = constants.SENSORS_NUMBER
+        angle_step = 180 / (sensors_number - 1) if sensors_number > 1 else 0
 
-        for i in range(self.sensors_number):
+        for i in range(sensors_number):
             angle = math.radians(-90 + i * angle_step)
-            end_x = math.cos(angle) * sensor_length
-            end_y = math.sin(angle) * sensor_length
+            end_x = math.cos(angle) * constants.SENSOR_LENGTH
+            end_y = math.sin(angle) * constants.SENSOR_LENGTH
 
             sensor_name = f"Sensor {i + 1}"
             sensor_shape = pymunk.Segment(
                 self.car_body,
                 (offset_distance, 0),
                 (offset_distance + end_x, end_y),
-                sensor_width)
+                constants.SENSOR_WIDTH)
             sensor_shape.sensor = True
-            sensor_shape.collision_type = 2
+            sensor_shape.collision_type = constants.SENSOR_COLLISION_TYPE
             sensor_shape.sensor_name = sensor_name
             self.space.add(sensor_shape)
             self.sensor_shapes.append(sensor_shape)
 
     def update(self, screen):
-        speed = 100
-        self.car_body.velocity = self.car_body.rotation_vector * speed
+        self.car_body.velocity = self.car_body.rotation_vector * constants.CAR_SPEED
         self.car_body.angular_velocity = 0
 
         # Draw car image
@@ -76,14 +74,15 @@ class Car:
         screen.blit(scaled_car_image, self.car_body.position - scaled_car_image.get_rect().center)
 
         # Draw all sensor lines
-        for sensor_shape in self.sensor_shapes:
-            # Calculate the sensor's absolute start and end points
-            sensor_start_pos = self.car_body.position + sensor_shape.a.rotated(self.car_body.angle)
-            sensor_end_pos = self.car_body.position + sensor_shape.b.rotated(self.car_body.angle)
+        if constants.DRAW_SENSORS:
+            for sensor_shape in self.sensor_shapes:
+                # Calculate the sensor's absolute start and end points
+                sensor_start_pos = self.car_body.position + sensor_shape.a.rotated(self.car_body.angle)
+                sensor_end_pos = self.car_body.position + sensor_shape.b.rotated(self.car_body.angle)
 
-            # Convert Pymunk positions to Pygame positions
-            sensor_start_pygame = pymunk.pygame_util.to_pygame(sensor_start_pos, screen)
-            sensor_end_pygame = pymunk.pygame_util.to_pygame(sensor_end_pos, screen)
+                # Convert Pymunk positions to Pygame positions
+                sensor_start_pygame = pymunk.pygame_util.to_pygame(sensor_start_pos, screen)
+                sensor_end_pygame = pymunk.pygame_util.to_pygame(sensor_end_pos, screen)
 
-            # Draw the sensor lines
-            pygame.draw.line(screen, (255, 0, 0), sensor_start_pygame, sensor_end_pygame, 2)
+                # Draw the sensor lines
+                pygame.draw.line(screen, (255, 0, 0), sensor_start_pygame, sensor_end_pygame, constants.SENSOR_WIDTH)
--- game_setup.py
+++ game_setup.py
@@ -1,11 +1,13 @@
 import pygame
 import pymunk
 
+import constants
+
 
 class GameSetup:
     def __init__(self):
         pygame.init()
-        self.width, self.height = self.get_screen_size()
+        self.width, self.height = constants.SCREEN_SIZE
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("PyMunk Car Game")
         self.space = self.create_space()
@@ -15,7 +17,3 @@ class GameSetup:
         space = pymunk.Space()
         space.gravity = (0, 0)
         return space
-
-    @staticmethod
-    def get_screen_size():
-        return 2000, 900
