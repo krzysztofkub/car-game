6de27bf523dee6e61b89d630f942eb59f694ec16
Oplomo <xplatsonqx@gmail.com>
2024-02-11 17:26:03 +0100

    2 tory do przetestowania

--- track.py
+++ track.py
@@ -11,11 +11,51 @@ class Track:
 
     @staticmethod
     def create_track(space, width, height, collision_type):
+        set_track_width = 200
+        track_width = set_track_width + 50
         static_lines = [
-            pymunk.Segment(space.static_body, (50, 50), (50, height - 50), 5),
-            pymunk.Segment(space.static_body, (50, height - 50), (width - 50, height - 50), 5),
-            pymunk.Segment(space.static_body, (width - 50, height - 50), (width - 50, 50), 5),
-            pymunk.Segment(space.static_body, (50, 50), (width - 50, 50), 5)
+
+            # TOR 1
+            # # Linia START/META:
+            #
+            # pymunk.Segment(space.static_body, (width/2, height-50), (width/2, height-track_width), 5),
+            #
+            # # Ramka zewnetrzna:
+            # pymunk.Segment(space.static_body, (50, 50), (50, height - 50), 5),
+            # pymunk.Segment(space.static_body, (50, height - 50), (width - 50, height - 50), 5),
+            # pymunk.Segment(space.static_body, (width - 50, height - 50), (width - 50, 50), 5),
+            # pymunk.Segment(space.static_body, (50, 50), (width - 50, 50), 5),
+            #
+            # # Ramka wewnetrzna:
+            # pymunk.Segment(space.static_body, (track_width, track_width), (track_width, height - track_width), 5),
+            # pymunk.Segment(space.static_body, (track_width, height - track_width),
+            #                (width - track_width, height - track_width), 5),
+            # pymunk.Segment(space.static_body, (width - track_width, height - track_width),
+            #                (width - track_width, track_width), 5),
+            # pymunk.Segment(space.static_body, (track_width, track_width), (width - track_width, track_width), 5),
+
+            ## TOR 2
+            ## Linia START:
+            # pymunk.Segment(space.static_body, (50, height - 50), (50, height - 300), 5),
+            ## Linia META:
+            # pymunk.Segment(space.static_body, (width - 50, height - 50), (width - 50, height - 300), 5),
+            ## Ramka:
+            # pymunk.Segment(space.static_body, (50, height - 50), (50, 50), 5),
+            # pymunk.Segment(space.static_body, (50, height - 50), (width - 50, height - 50), 5),
+            # pymunk.Segment(space.static_body, (width - 50, height - 50), (width - 50, 50), 5),
+            # pymunk.Segment(space.static_body, (50, 50), (width - 50, 50), 5),
+
+            ## Przeszkody:
+            # pymunk.Segment(space.static_body, (200, height - 200), (200, 50), 5),
+            # pymunk.Segment(space.static_body, (400, height - 50), (400, 200), 5),
+            # pymunk.Segment(space.static_body, (600, height - 200), (600, 50), 5),
+            # pymunk.Segment(space.static_body, (800, height - 50), (800, 200), 5),
+            # pymunk.Segment(space.static_body, (1000, height - 200), (1000, 50), 5),
+            # pymunk.Segment(space.static_body, (1200, height - 50), (1200, 200), 5),
+            # pymunk.Segment(space.static_body, (1400, height - 200), (1400, 50), 5),
+            # pymunk.Segment(space.static_body, (1600, height - 50), (1600, 200), 5),
+            # pymunk.Segment(space.static_body, (1800, height - 200), (1800, 50), 5),
+
         ]
 
         for i, line in enumerate(static_lines):
